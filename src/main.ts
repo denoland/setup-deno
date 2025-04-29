bd95779da6b2a625a22d9dd1c095b145cb5c711b
Christian Svensson <csvn.dev@gmail.com>
2025-04-29 10:43:39 +0200

    refactor: convert action to TS

---
+++ src/main.ts
@@ -0,0 +1,65 @@
+import process from "node:process";
+import core from "@actions/core";
+import path from "node:path";
+import {
+  getDenoVersionFromFile,
+  parseVersionRange,
+  resolveVersion,
+} from "./version.ts";
+import { install } from "./install.ts";
+
+declare global {
+  interface ImportMeta {
+    dirname?: string;
+    filename?: string;
+  }
+}
+
+function exit(message: string): never {
+  core.setFailed(message);
+  process.exit();
+}
+
+async function main() {
+  try {
+    const denoVersionFile = core.getInput("deno-version-file");
+    const range = parseVersionRange(
+      denoVersionFile
+        ? getDenoVersionFromFile(denoVersionFile)
+        : core.getInput("deno-version"),
+    );
+
+    if (range === null) {
+      exit("The passed version range is not valid.");
+    }
+
+    const version = await resolveVersion(range);
+    if (version === null) {
+      exit("Could not resolve a version for the given range.");
+    }
+
+    core.info(`Going to install ${version.kind} version ${version.version}.`);
+
+    await install(version);
+
+    core.info(
+      `::add-matcher::${
+        path.join(
+          import.meta.dirname ?? ".",
+          "..",
+          "deno-problem-matchers.json",
+        )
+      }`,
+    );
+
+    core.setOutput("deno-version", version.version);
+    core.setOutput("release-channel", version.kind);
+
+    core.info("Installation complete.");
+  } catch (err) {
+    core.setFailed((err instanceof Error) ? err : String(err));
+    process.exit();
+  }
+}
+
+main();
